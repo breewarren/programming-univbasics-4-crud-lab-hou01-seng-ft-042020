c90d299e9c9f878e2b439ab4826aea5c2c119a0c
Bree Warren <ambreea.warren@gmail.com>
2020-03-03 01:12:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = [0, 1, 2, 3, 4, "arrays!"]
+  array [-1]
 end
 
 def update_element_from_index(array, index_number, element)
