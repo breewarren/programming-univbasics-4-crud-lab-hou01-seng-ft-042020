c965500c71768dc2c0f3053408956150d083f245
Bree Warren <ambreea.warren@gmail.com>
2020-03-03 01:14:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,5 +44,7 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = [0, 1, 2, 3, 4, 5]
+  index_number = []
+  element = "totally"
 end
